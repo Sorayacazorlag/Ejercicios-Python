b37d8927dd33f334133ce8509ef334eb4c146c0e
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-20 13:12:29 +0200

    EJERCICIOS TEMA 1

---
+++ Ejercicios/t1e1peliculas.py
@@ -0,0 +1,13 @@
+favourite_film = "Trainspotting"
+film_director = "Danny Boyle"
+release_year = 1996
+genre = "Drama"
+duration = 90
+awards = True
+
+print("Favourite film", favourite_film)
+print("Director", film_director)
+print("Release year", release_year)
+print("Genre", genre)
+print("Duration", duration)
+print("Awards", awards)
