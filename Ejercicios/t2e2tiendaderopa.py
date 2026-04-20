22ec33e00f4f993b10880b5ce967c19d4f7782aa
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-20 16:49:56 +0200

    Ejercicio tienda de ropa python

---
+++ Ejercicios/t2e2tiendaderopa.py
@@ -0,0 +1,19 @@
+camiseta = 10
+sudadera = 20.5
+gorra = 5.5
+
+cantidad_camisetas = int(input("¿Cuántas camisetas llevas?"))
+cantidad_sudaderas = int(input("¿Cuántas sudaderas llevas?"))
+cantidad_gorras = int(input("¿Cuántas gorras llevas?"))
+
+total_compra = float((cantidad_camisetas * camiseta) + (cantidad_sudaderas * sudadera) + (cantidad_gorras * gorra))
+total_mas_21iva = float(total_compra * 1.21)
+
+print("Precio camiseta" , camiseta)
+print("Precio sudadera" , sudadera)
+print("Precio gorra" , gorra)
+
+
+print("Precio total sin IVA" , total_compra)
+print("Precio total con 21%IVA" , total_mas_21iva)
+
