8af2304a2471e4165165c97863f2776b74bd308a
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-21 17:08:09 +0200

    t2e3

---
+++ Ejercicios/t2e3frases.py
@@ -0,0 +1,12 @@
+frase = input("Introduzca una frase" )
+
+longitud = len(frase)
+mayusculas = frase.upper()
+minusculas = frase.lower()
+
+print(frase)
+print("Longitud", longitud)
+print("Frase en mayusculas", mayusculas)
+print("Frase en minusculas", minusculas)
+
+
