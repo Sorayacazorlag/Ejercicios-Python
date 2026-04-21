94f91fd673b21e4a449c35425665a8e89d1b929d
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-21 17:16:11 +0200

    T2E4

---
+++ Ejercicios/t2e4correoelectronico.py
@@ -0,0 +1,10 @@
+email = input("Introduzca un correo electrónico" )
+
+longitud = len(email)
+mayusculas = email.upper()
+minusculas = email.lower()
+
+print("Longitud", longitud)
+print("Email en mayúsculas", mayusculas)
+print("Email en minúsculas", minusculas)
+
