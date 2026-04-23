2c1ed59b8431a165fd2a36442c2b3c23674cfe80
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-23 12:45:37 +0200

    Update t2e2tiendaderopa.py

--- Ejercicios/t2e2tiendaderopa.py
+++ Ejercicios/t2e2tiendaderopa.py
@@ -1,13 +1,18 @@
+#Precio artículos
 camiseta = 10
 sudadera = 20.5
 gorra = 5.5
 
-cantidad_camisetas = int(input("¿Cuántas camisetas llevas?"))
-cantidad_sudaderas = int(input("¿Cuántas sudaderas llevas?"))
-cantidad_gorras = int(input("¿Cuántas gorras llevas?"))
+cantidad_camisetas = int(input("¿Cuántas camisetas llevas? "))
+cantidad_sudaderas = int(input("¿Cuántas sudaderas llevas? "))
+cantidad_gorras = int(input("¿Cuántas gorras llevas? "))
 
-total_compra = (cantidad_camisetas * camiseta) + (cantidad_sudaderas * sudadera) + (cantidad_gorras * gorra)
-total_mas_21iva = total_compra * 1.21
+precio_total_camisetas = cantidad_camisetas * camiseta
+precio_total_sudaderas = cantidad_sudaderas * sudadera
+precio_total_gorras = cantidad_gorras * gorra
+
+total_compra = precio_total_camisetas + precio_total_sudaderas + precio_total_gorras
+total_mas_21iva = total_compra + (total_compra * 0.21)
 
 print("Precio camiseta" , camiseta)
 print("Precio sudadera" , sudadera)
