8ff6c2ddc48d36dd76fe0a0efd15c4c5fee81a46
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-23 13:16:38 +0200

    Update t3e2conversordemonedas.py

--- Ejercicios/t3e2conversordemonedas.py
+++ Ejercicios/t3e2conversordemonedas.py
@@ -1,10 +1,24 @@
-cantidad_euros = float(input("Introduzca una cantidad en euros: "))
+#Convertir euros a dólares y libras
 
-def calcular_conversiones (conversion_dolares, conversion_libras):
-    dolares = conversion_dolares * cantidad_euros
-    libras = conversion_libras * cantidad_euros
-    return dolares, libras
+def convertir_euros_a_dolares(cantidad_euros):
+    resultado = cantidad_euros * 1.1
+    return resultado
 
-dolares, libras = calcular_conversiones (1.1, 0.87)
-print("Dolares: ", dolares)
-print("Libras: ", libras)
+def convertir_euros_a_libras(cantidad_euros):
+    resultado = cantidad_euros * 0.87
+    return resultado
+
+#Pide a la usuaria la cantidad de euros a cambiar
+
+def conversor():
+    cantidad_en_euros = float(input("¿Cuántos euros quiere cambiar? "))
+
+    print("Conversión a dólares: ")
+    cantidad_en_dolares = convertir_euros_a_dolares(cantidad_en_euros)
+    print(cantidad_en_dolares)
+
+    print("Conversión a libras: ")
+    cantidad_en_libras = convertir_euros_a_libras(cantidad_en_euros)
+    print(cantidad_en_libras)
+
+conversor()
