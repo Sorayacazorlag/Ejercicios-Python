8f6d1f48da2ea20d745ecf9d87113f190924d649
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-23 13:19:27 +0200

    Update t3e1funciones.py

--- Ejercicios/t3e1funciones.py
+++ Ejercicios/t3e1funciones.py
@@ -1,24 +1,28 @@
-#Solicitar datos a la usuaria
-nombre_producto = input("Introduzca el nombre del producto: ")
-precio_unidad = input("Introduzca el precio de la unidad: ")
-cantidad = input("Introduzca la cantidad de productos: ")
-descuento_porcentaje = int(input("Introduzca el descuento en porcentaje: "))
-iva_porcentaje = int(input("Introduzca el IVA en porcentaje: "))
-
 #Función calcular el precio total aplicando descuento e IVA
-def calcular_total_producto(precio_unidad, cantidad)
-    total_producto = precio_unidad * cantidad
-    return total_producto
+def calcular_total_producto(precio, cantidad):
+    precio_total = precio * cantidad
+    return precio_total
 
-def calcular_descuento(total_producto, descuento_porcentaje)
-    total_descuento = total_producto - (total_producto)
+def calcular_descuento(total, porcentaje_descuento):
+    total_descuento = total - (total * porcentaje_descuento)/100
+    return total_descuento
 
+def calcular_iva(total, iva):
+    total_iva = total + (total * iva)/100
+    return total_iva
 
+def calcular_operaciones():
+    #Solicitud parámetros a la usuaria
+    nombre_del_producto = input("¿Cuál es el nombre del artículo? ")
+    precio_unidad = float(input("¿Cuál es el precio de 1 unidad? "))
+    cantidad_a_comprar = int(input("¿Cuántos artículos llevas? "))
+    descuento_en_porcentaje = float(input("¿Qué porcentaje de descuento tiene? "))
+    iva_en_porcentaje = float(input("¿Qué porcentaje de IVA tiene? "))
 
-def calcular_descuento(descuento_porcentaje, cienporcien):
-    descuento = cantidad * precio_unidad * (descuento_porcentaje / cienporcien)
-    return descuento
+    total_compra = calcular_total_producto(precio_unidad, cantidad_a_comprar)
+    total_descuento = calcular_descuento(total_compra, descuento_en_porcentaje)
+    total_iva = calcular_iva(total_descuento, iva_en_porcentaje)
 
-descuento_total = descuento (20, 100)
+    print("El total de la compra del producto " + nombre_del_producto + " es ", + total_iva), "euros"
 
-print("Descuento total: ", descuento)
+calcular_operaciones()
