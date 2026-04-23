d63bcc514f2a9a14703e64b571f8e4f8236f3b49
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-23 12:38:45 +0200

    Update t1e2canciones.py

--- Ejercicios/t1e2canciones.py
+++ Ejercicios/t1e2canciones.py
@@ -1,9 +1,11 @@
-cancion_favorita = input("¿Cuál es tu canción favorita?")
-artista = input("¿De qué artista es?")
-album = input("¿Pertenece a algún álbum? Si la respuesta es si, ¿a cuál?")
-anyo_lanzamiento = int(input("¿Cuál fue el año de lanzamiento?"))
-duracion = float(input("¿Cuánto dura en segundos?"))
-videoclip = input("¿Tiene videoclip?")
+#Pide a la usuaria que intrduzca la siguiente información sobre su canción favorita
+
+cancion_favorita = input("¿Cuál es tu canción favorita? ")
+artista = input("¿De qué artista es? ")
+album = input("¿Pertenece a algún álbum? Si la respuesta es si, ¿a cuál? ")
+anyo_lanzamiento = int(input("¿Cuál fue el año de lanzamiento? "))
+duracion = float(input("¿Cuánto dura en segundos? "))
+videoclip = input("¿Tiene videoclip? ")
 
 print("Mi canción favorita es", cancion_favorita)
 print("Pertenece al artista/banda", artista)
@@ -12,12 +14,13 @@ print("Salió en el año", anyo_lanzamiento)
 print("Tiene una duración de", duracion)
 print("¿Tiene videoclip?", videoclip)
 
-peor_cancion = input("¿Cuál es la canción que menos te gusta?")
-peor_artista = input("¿De qué artista es?")
-peor_album = input("¿Pertenece a algún álbum? Si la respuesta es si, ¿a cuál?")
-peor_anyo_lanzamiento = int(input("¿Cuál fue el año de lanzamiento?"))
-peor_duracion = float(input("¿Cuánto dura en segundos?"))
-peor_videoclip = input("¿Tiene videoclip?")
+#Pide a la usuaria que introduzca la siguiente información sobre la canción que menos le gusta
+peor_cancion = input("¿Cuál es la canción que menos te gusta? ")
+peor_artista = input("¿De qué artista es? ")
+peor_album = input("¿Pertenece a algún álbum? Si la respuesta es si, ¿a cuál? ")
+peor_anyo_lanzamiento = int(input("¿Cuál fue el año de lanzamiento? "))
+peor_duracion = float(input("¿Cuánto dura en segundos? "))
+peor_videoclip = input("¿Tiene videoclip? ")
 
 print("La canción que menos me gusta es", peor_cancion)
 print("Pertenece al artista/banda", peor_artista)
