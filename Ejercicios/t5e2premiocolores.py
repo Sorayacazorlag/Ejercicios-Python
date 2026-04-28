0799d0bbe7ce78a16582fece4ca5910a357e3546
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-04-28 10:27:44 +0200

    Create t5e2premiocolores.py

---
+++ Ejercicios/t5e2premiocolores.py
@@ -0,0 +1,11 @@
+#Pide a la usuaria 5 veces que introduzca un color
+#Si la usuaria introduce el color azul, muestra el premio conseguido.
+#Si no, dile que pruebe otro color
+
+color = input("Introduce un color: ").lower()
+
+for i in range(color):
+    if color == azul:
+        break
+    print("¡HA CONSEGUIDO EL PREMIO!")
+
